036886039fbe6c5584dd5fee1b1a19bd37409015
William Kamp <manimaul@gmail.com>
2018-04-06 18:11:05 -0700

    another sandbox server

---
+++ src/main/java/com/willkamp/sandbox/NettyHttpServer.java
@@ -0,0 +1,98 @@
+package com.willkamp.sandbox;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.*;
+
+import static io.netty.buffer.Unpooled.copiedBuffer;
+
+public class NettyHttpServer {
+    private ChannelFuture channel;
+    private final EventLoopGroup masterGroup;
+    private final EventLoopGroup slaveGroup;
+
+    private NettyHttpServer() {
+        masterGroup = new NioEventLoopGroup();
+        slaveGroup = new NioEventLoopGroup();
+    }
+
+    private void start() {
+        Runtime.getRuntime().addShutdownHook(new Thread(this::shutdown));
+
+        try {
+            final ServerBootstrap bootstrap = new ServerBootstrap()
+                    .group(masterGroup, slaveGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(final SocketChannel ch) {
+                            ch.pipeline().addLast("codec", new HttpServerCodec());
+                            ch.pipeline().addLast("aggregator", new HttpObjectAggregator(512 * 1024));
+                            ch.pipeline().addLast("request", new ChannelInboundHandlerAdapter() {
+                                @Override
+                                public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                                    if (msg instanceof FullHttpRequest) {
+                                        final FullHttpRequest request = (FullHttpRequest) msg;
+
+                                        final String responseMessage = "Hello from Netty!";
+
+                                        FullHttpResponse response = new DefaultFullHttpResponse(
+                                                HttpVersion.HTTP_1_1,
+                                                HttpResponseStatus.OK,
+                                                copiedBuffer(responseMessage.getBytes())
+                                        );
+
+                                        if (HttpUtil.isKeepAlive(request)) {
+                                            response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+                                        }
+                                        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+                                        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, responseMessage.length());
+
+                                        ctx.writeAndFlush(response);
+                                    } else {
+                                        ctx.fireChannelRead(msg);
+                                    }
+                                }
+
+                                @Override
+                                public void channelReadComplete(ChannelHandlerContext ctx) {
+                                    ctx.flush(); // request to flush all pending messages via this ChannelOutboundInvoker.
+                                }
+
+                                @Override
+                                public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+                                    ctx.writeAndFlush(new DefaultFullHttpResponse(
+                                            HttpVersion.HTTP_1_1,
+                                            HttpResponseStatus.INTERNAL_SERVER_ERROR,
+                                            copiedBuffer(cause.getMessage().getBytes())
+                                    ));
+                                }
+                            });
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128) // refused connections after queue reaches 128
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // allows keep alive
+            channel = bootstrap.bind(8080).sync();
+        } catch (final InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void shutdown() {
+        slaveGroup.shutdownGracefully();
+        masterGroup.shutdownGracefully();
+
+        try {
+            channel.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new NettyHttpServer().start();
+    }
+}
